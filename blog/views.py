9e6f0be34f394f796c64f3ca13d10cd979c399de
frencita03 <frencita03@gmail.com>
2017-09-04 17:06:23 +0530

    Displaying posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.contrib.auth.models import User
+
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    me=User.objects.get(username='frencita')
+    published_date__lte=timezone.now()
+    posts = Post.objects.filter(author=me).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
